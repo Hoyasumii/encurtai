3ffcea649d9f352f04d0225d1ac1aa6376c64377
Hoyasumii <alanreisanjo@gmail.com>
2024-10-10 14:07:45 -0300

    Ajustes da tela de loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import { LoaderCircle } from "lucide-react";
 
 export default function Page() {
   return (
-    <div className="absolute top-0 w-full h-full flex justify-center items-center bg-white backdrop-blur">
+    <div className="absolute top-0 w-full h-full flex justify-center items-center bg-slate-50 dark:bg-stone-900">
       <LoaderCircle className="animate-spin" />
     </div>
   );
